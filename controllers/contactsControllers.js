3128c7ed726c4afee603c75a869cb2830cc1336e
PMichelangelo <Pmichelangelo1377@gmail.com>
2024-03-19 11:58:36 +0200

    commit decorators

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,61 +1,48 @@
 import * as contactsService from "../services/contactsServices.js";
 
+import cntrlWrapper from "../decorators/cntrlWrapper.js";
+
 import HttpError from "../helpers/HttpError.js"
 
 import { createContactSchema, updateContactSchema } from "../schemas/contactsSchemas.js";
 
-export const getAllContacts = async (req, res, next) => {
-    try {
+ const getAllContacts = async (req, res) => {
         const results = await contactsService.listContacts()
-
         res.json(results)
-    } catch (error) {
-        next(error)
-    }
 
 };
 
-export const getOneContact = async(req, res, next) => {
-    try {
+ const getOneContact = async(req, res) => {
         const { id } = req.params
         const result = await contactsService.getContactById(id)
         if (!result) {
             throw HttpError(404,"Not found")
         }
         res.json(result)
-    } catch (error) {
-        next(error)
-    }
+
 };
 
-export const deleteContact = async (req, res, next) => {
-    try {
+const deleteContact = async (req, res) => {
         const { id } = req.params
         const result = await contactsService.removeContact(id)
         if (!result) {
             throw HttpError(404, "Not found");
         }
         res.status(200).json(result)
-    } catch (error) {
-        next(error)
-    }
+
 };
 
-export const createContact = async (req, res, next) => {
-    try {
+const createContact = async (req, res) => {
         const {error} = createContactSchema.validate(req.body)
         if (error) {
             throw HttpError(400,error.message)
         }
         const result = await contactsService.addContact(req.body)
         res.status(201).json(result)
-    } catch (error) {
-        next(error)
-    }
+
 };
 
-export const updateContact = async (req, res, next) => {
-    try {
+const updateContact = async (req, res) => {
         if (Object.keys(req.body).length === 0) {
             throw HttpError(400, "Body must have at least one field")
         }
@@ -70,8 +57,13 @@ export const updateContact = async (req, res, next) => {
         }
 
         res.json(result);
-    }
-    catch(error) {
-        next(error);
-    }
+
+}
+
+export default {
+    getAllContacts: cntrlWrapper(getAllContacts),
+    getOneContact: cntrlWrapper(getOneContact),
+    deleteContact: cntrlWrapper(deleteContact),
+    createContact: cntrlWrapper(createContact),
+    updateContact: cntrlWrapper(updateContact)
 }
